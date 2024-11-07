4a016c48e9807d6e778fa192a7a71540bb8d0043
wesleyconstantino2004 <108415124+wesleyconstantino2004@users.noreply.github.com>
2024-11-07 08:59:34 -0300

    Add files via upload

---
+++ quantidadeUsuarios.js
@@ -0,0 +1,33 @@
+import { getCSS } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/Faccinmateus/Avalia-o-4/refs/heads/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+
+quantidadeUsuariosPorRede()
